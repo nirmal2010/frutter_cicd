c366b572423e8a5f3a79c7e73b3197eeeca47154
nirmalrajaa@frutterlabs.com <nirmalrajaa@frutterlabs.com>
2024-05-16 15:23:42 +0530

    Doctype added

---
+++ frutter_cicd/frutter_cicd/doctype/frutter_cicd/frutter_cicd.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nirmalrajaa@frutterlabs.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Frutter CICD", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ frutter_cicd/frutter_cicd/doctype/frutter_cicd/test_frutter_cicd.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, nirmalrajaa@frutterlabs.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFrutterCICD(FrappeTestCase):
+	pass
