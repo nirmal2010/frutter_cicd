8ce6fe774530727adbe697299b120865c0ef3aeb
nirmalrajaa@frutterlabs.com <nirmalrajaa@frutterlabs.com>
2024-05-17 14:44:20 +0530

    Added test case to Doctype test

--- frutter_cicd/frutter_cicd/doctype/frutter_cicd/test_frutter_cicd.py
+++ frutter_cicd/frutter_cicd/doctype/frutter_cicd/test_frutter_cicd.py
@@ -1,24 +1,45 @@
 # Copyright (c) 2024, nirmalrajaa@frutterlabs.com and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 from frutter_cicd.doctype.frutter_cicd.frutter_cicd import FrutterCICD
 
 
-class TestFrutterCICD(FrappeTestCase):
+# class TestFrutterCICD(FrappeTestCase):
 
-	def test_frutter_cicd_document_creation(self):
-		# Create a new FrutterCICD document
-		frutter_cicd = FrutterCICD()
-		frutter_cicd.title = "Test Document"
-		frutter_cicd.save()
+# 	def test_frutter_cicd_document_creation(self):
+# 		# Create a new FrutterCICD document
+# 		frutter_cicd = FrutterCICD()
+# 		frutter_cicd.title = "Test Document"
+# 		frutter_cicd.save()
 
-		# Retrieve the document from the database
-		retrieved_frutter_cicd = FrutterCICD.get(frutter_cicd.name)
+# 		# Retrieve the document from the database
+# 		retrieved_frutter_cicd = FrutterCICD.get(frutter_cicd.name)
 
-		# Assert that the retrieved document is not None
-		self.assertIsNotNone(retrieved_frutter_cicd)
+# 		# Assert that the retrieved document is not None
+# 		self.assertIsNotNone(retrieved_frutter_cicd)
 
-		# Assert that the title of the retrieved document matches the original title
-		self.assertEqual(retrieved_frutter_cicd.title, "Test Document")
+# 		# Assert that the title of the retrieved document matches the original title
+# 		self.assertEqual(retrieved_frutter_cicd.title, "Test Document")
+
+
+import unittest
+
+def sum_of_two_numberss(a, b):
+    return a + b
+
+class TestSums(unittest.TestCase):
+    def test_sum_of_two_numbers(self):
+        # Arrange
+        a = 5
+        b = 9
+
+        # Act
+        result = sum_of_two_numberss(a, b)
+
+        # Assert
+        self.assertEqual(result, 17)
+
+if __name__ == '__main__':
+    unittest.main()
