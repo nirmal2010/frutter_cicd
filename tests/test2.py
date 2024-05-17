44ed7fa0d5773e8874e0227367dfd794051eb9c4
nirmalrajaa@frutterlabs.com <nirmalrajaa@frutterlabs.com>
2024-05-17 12:11:22 +0530

    the old test case and new one

--- tests/test2.py
+++ tests/test2.py
@@ -1,16 +1,16 @@
 import unittest
 
-def sum_of_two_numbers(a, b):
+def sum_of_two_numberss(a, b):
     return a + b
 
-class TestSum(unittest.TestCase):
+class TestSums(unittest.TestCase):
     def test_sum_of_two_numbers(self):
         # Arrange
         a = 5
         b = 7
 
         # Act
-        result = sum_of_two_numbers(a, b)
+        result = sum_of_two_numberss(a, b)
 
         # Assert
         self.assertEqual(result, 13)
